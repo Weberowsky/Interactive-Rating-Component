2333df5544306bf8186473a8307c7491d886a885
Weberowsky <36982628+Weberowsky@users.noreply.github.com>
2023-05-01 14:41:15 +0200

    Just need to fix the styles a lil bit

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 const buttons = document.querySelectorAll(".number-button")
-let score 
+let score
 buttons.forEach(btn => btn.addEventListener("click", function(e) {
-    e = e || window.event;
-    if (document.querySelector(".button-selected")) {
-        document.querySelector(".button-selected").className = "number-button"
-    }
-    e.target.className = "button-selected"
-    score = e.target.innerHTML
+e = e || window.event;
+if (e.target.classList.contains("button-selected")) {
+e.target.classList.remove("button-selected");
+score = undefined;
+} else {
+if (document.querySelector(".button-selected")) {
+document.querySelector(".button-selected").classList.remove("button-selected");
+}
+e.target.classList.add("button-selected");
+score = e.target.innerHTML;
+}
 }));
 
 document.querySelector("#submit").addEventListener("click", function(e) {
-    e = e || window.event;
-    if (score) {
-        document.getElementById("customer-feedback").style.display = "none"
-        document.getElementById("thank-you").style.display = "flex"
-        document.getElementById("number").innerHTML = `You selected ${score} out of 5`
-    }
+e = e || window.event;
+if (score) {
+document.getElementById("customer-feedback").style.display = "none"
+document.getElementById("thank-you").style.display = "flex"
+document.getElementById("number").innerHTML = `You selected ${score} out of 5`
+}
 })
